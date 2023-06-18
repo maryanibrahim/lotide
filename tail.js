5a98648f972e782a141af4a4fd79571883853b8b
maryanibrahim <maryanibrahim301@yahoo.com>
2023-06-18 20:38:13 +0000

    retrieves every element except the head (first element) of an array.

---
+++ tail.js
@@ -0,0 +1,11 @@
+/* eslint-disable func-names */
+/* eslint-disable no-plusplus */
+const tail = function (array) {
+  const tailArray = [];
+  for (let i = 1; i < array.length; i++) {
+    tailArray.push(array[i]);
+  }
+  return tailArray;
+};
+
+module.exports = tail;
