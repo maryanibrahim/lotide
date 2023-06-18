ab73a252365e8c3107e2625a16817e43ca521291
maryanibrahim <maryanibrahim301@yahoo.com>
2023-06-18 20:13:20 +0000

    checks if two arrays are equal, accounting for nested arrays.

---
+++ eqArrays.js
@@ -0,0 +1,15 @@
+/* eslint-disable func-names */
+const eqArrays = function (array1, array2) {
+  let output = true;
+  if (array1.length !== array2.length) return false;
+  for (let element = 0; element < array1.length; element += 1) {
+    if (Array.isArray(array1[element]) || Array.isArray(array2[element])) {
+      output = output && eqArrays(array1[element], array2[element]);
+    } else if (array1[element] !== array2[element]) {
+      output = output && false;
+    }
+  }
+  return output;
+};
+
+module.exports = eqArrays;
