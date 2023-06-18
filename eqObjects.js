cd6e0294f8dc48cd477b6daa8c3fac020f007fe2
maryanibrahim <maryanibrahim301@yahoo.com>
2023-06-18 20:14:20 +0000

    checks if two objects are equal, accounting for nested objects and arrays.

---
+++ eqObjects.js
@@ -0,0 +1,22 @@
+/* eslint-disable func-names */
+/* eslint-disable no-restricted-syntax */
+const eqArrays = require('./eqArrays');
+
+const eqObjects = function (object1, object2) {
+  let output = true;
+  const array1 = Object.keys(object1);
+  const array2 = Object.keys(object2);
+  if (array1.length !== array2.length) output = output && false;
+  else {
+    for (const key in object1) {
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        output = output && (eqArrays(object1[key], object2[key]));
+      } else if (typeof (object1[key]) === 'object' || typeof (object2[key]) === 'object') {
+        output = output && eqObjects(object1[key], object2[key]);
+      } else if (object1[key] !== object2[key]) output = output && false;
+    }
+  }
+  return output;
+};
+
+module.exports = eqObjects;
