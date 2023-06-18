ad51d122435ffdc190e3ce2da3dbaeb4b39c9989
maryanibrahim <maryanibrahim301@yahoo.com>
2023-06-18 20:34:36 +0000

    generates an object that stores the indices of each letter in a given sentence.

---
+++ letterPositions.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable func-names */
+const letterPositions = function (sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    const letter = sentence[i];
+    if (letter !== ' ') {
+      if (!results[letter]) {
+        results[letter] = [];
+      }
+      results[letter].push(i);
+    }
+  }
+  return results;
+};
+module.exports = letterPositions;
