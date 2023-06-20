9eec6d12abe614450cd259981a3ce13780fadc0d
maryanibrahim <maryanibrahim301@yahoo.com>
2023-06-20 06:31:18 +0000

    assertEqualTest

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,16 +1,14 @@
-
-//const assertEqual = require('../assertEqual');
-//const assertEqual = require (../assertEqual);
+const assertEqual = require('../assertEqual');
 
 // TEST CODE
 // Comparing identical strings
-//assertEqual('Hello', 'Hello');
+assertEqual('Hello', 'Hello');
 
 // Comparing non-identical strings
-//assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual('Lighthouse Labs', 'Bootcamp');
 
-// Comparing identical numbers/
-//assertEqual(42, 42);
+// Comparing identical numbers
+assertEqual(42, 42);
 
 // Comparing non-identical numbers
-//assertEqual(1, 2);
+assertEqual(1, 2);
